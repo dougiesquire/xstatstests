3ed0be0d0dc525e1d8548bd746239b4c8971b16b
dougiesquire <dougiesquire@gmail.com>
2021-12-07 16:10:42 +1100

    test dask arrays

--- xks/tests/fixtures.py
+++ xks/tests/fixtures.py
@@ -1,13 +1,35 @@
 import numpy as np
 import xarray as xr
 
+import dask
+import dask.array as dsa
 
-def ds_1var(shape):
+
+def empty_dask_array(shape, dtype=float, chunks=None):
+    """A dask array that errors if you try to compute it
+    Stolen from https://github.com/xgcm/xhistogram/blob/master/xhistogram/test/fixtures.py
+    """
+
+    def raise_if_computed():
+        raise ValueError("Triggered forbidden computation on dask array")
+
+    a = dsa.from_delayed(dask.delayed(raise_if_computed)(), shape, dtype)
+    if chunks is not None:
+        a = a.rechunk(chunks)
+    return a
+
+
+def ds_1var(shape, dask=False):
     """An example DataSet.
     The first dimension is named 'sample' and the data is identical over other dimensions
     """
     coords = [range(s) for s in shape]
     dims = ["sample"] + [f"dim_{i}" for i in range(1, len(shape))]
-    data = np.expand_dims(np.random.random(size=shape[0]), list(range(1, len(shape))))
-    data = np.tile(data, (*shape[1:],))
+    if dask:
+        data = empty_dask_array(shape)
+    else:
+        data = np.expand_dims(
+            np.random.random(size=shape[0]), list(range(1, len(shape)))
+        )
+        data = np.tile(data, (*shape[1:],))
     return xr.DataArray(data, coords=coords, dims=dims).to_dataset(name="var")
--- xks/tests/test_core.py
+++ xks/tests/test_core.py
@@ -13,17 +13,19 @@ from scipy.stats import ks_2samp
 @pytest.mark.parametrize("ds1_samples", [1000])
 @pytest.mark.parametrize("ds2_samples", [100, 1000])
 @pytest.mark.parametrize("shape", [(), (2,), (2, 3)])
-def test_ks1d2s(ds1_samples, ds2_samples, shape):
+@pytest.mark.parametrize("dask", [True, False])
+def test_ks1d2s(ds1_samples, ds2_samples, shape, dask):
     """Test values of ks1d2s relative to scipy.stats.ks_2samp"""
-    ds1 = ds_1var((ds1_samples,) + shape)
-    ds2 = ds_1var((ds2_samples,) + shape)
+    ds1 = ds_1var((ds1_samples,) + shape, dask)
+    ds2 = ds_1var((ds2_samples,) + shape, dask)
     D_res, p_res = ks1d2s(ds1, ds2, sample_dim="sample")
-    ds1_1d = np.reshape(ds1["var"].values, (ds1.sizes["sample"], -1))[:, 0]
-    ds2_1d = np.reshape(ds2["var"].values, (ds2.sizes["sample"], -1))[:, 0]
-    D_ver, p_ver = ks_2samp(ds1_1d, ds2_1d)
+    if dask is False:
+        ds1_1d = np.reshape(ds1["var"].values, (ds1.sizes["sample"], -1))[:, 0]
+        ds2_1d = np.reshape(ds2["var"].values, (ds2.sizes["sample"], -1))[:, 0]
+        D_ver, p_ver = ks_2samp(ds1_1d, ds2_1d)
 
-    npt.assert_allclose(D_res["var"].values, D_ver)
-    npt.assert_allclose(p_res["var"].values, p_ver)
+        npt.assert_allclose(D_res["var"].values, D_ver)
+        npt.assert_allclose(p_res["var"].values, p_ver)
 
 
 @pytest.mark.parametrize("samples", [100, 1000])
