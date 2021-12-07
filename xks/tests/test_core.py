15f54c80a18ea57ea28a975ddc72b8c09fbac2eb
dougiesquire <dougiesquire@gmail.com>
2021-12-07 15:34:50 +1100

    add test for ks2d2s

--- xks/tests/test_core.py
+++ xks/tests/test_core.py
@@ -3,7 +3,9 @@ import pytest
 import numpy as np
 import numpy.testing as npt
 
-from xks import ks1d2s
+import xarray as xr
+
+from xks import ks1d2s, ks2d2s
 from .fixtures import ds_1var
 from scipy.stats import ks_2samp
 
@@ -11,8 +13,8 @@ from scipy.stats import ks_2samp
 @pytest.mark.parametrize("ds1_samples", [1000])
 @pytest.mark.parametrize("ds2_samples", [100, 1000])
 @pytest.mark.parametrize("shape", [(), (2,), (2, 3)])
-def test_ks1d2s_1D(ds1_samples, ds2_samples, shape):
-    """Test values of ks1d2s"""
+def test_ks1d2s(ds1_samples, ds2_samples, shape):
+    """Test values of ks1d2s relative to scipy.stats.ks_2samp"""
     ds1 = ds_1var((ds1_samples,) + shape)
     ds2 = ds_1var((ds2_samples,) + shape)
     D_res, p_res = ks1d2s(ds1, ds2, sample_dim="sample")
@@ -22,3 +24,16 @@ def test_ks1d2s_1D(ds1_samples, ds2_samples, shape):
 
     npt.assert_allclose(D_res["var"].values, D_ver)
     npt.assert_allclose(p_res["var"].values, p_ver)
+
+
+@pytest.mark.parametrize("samples", [100, 1000])
+@pytest.mark.parametrize("shape", [(), (2,), (2, 3)])
+def test_ks2d2s_identical(samples, shape):
+    """Check that KS statistical is zero for identical arrays"""
+    ds1_v1 = ds_1var((samples,) + shape).rename({"var": "var_1"})
+    ds1_v2 = ds_1var((samples,) + shape).rename({"var": "var_2"})
+    ds1 = xr.merge([ds1_v1, ds1_v2])
+    ds2 = ds1.copy()
+    D = ks2d2s(ds1, ds2, "sample")
+
+    npt.assert_allclose(D.values, 0.0)
